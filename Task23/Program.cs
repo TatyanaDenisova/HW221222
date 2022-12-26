87199d8026617342b124f058a3a9bbf22f966ae3
Tatiana Denisova <2746619@gmail.com>
2022-12-26 17:59:38 +0300

    add task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int getUserValue (string message)
+{
+    Console.WriteLine(message);
+    int resalt = int.Parse(Console.ReadLine()!);
+    return resalt;
+}
+void kub (int N)
+{
+    for (int i = 1; i < N + 1; i++)
+{
+    int temp = i*i*i;
+    if(i < N)
+    {
+        Console.Write($" {temp},");
+    }
+    else
+    {
+        Console.Write($" {temp}");
+    }
+}
+}
+int N = getUserValue("Введите число N:");
+kub(N);
